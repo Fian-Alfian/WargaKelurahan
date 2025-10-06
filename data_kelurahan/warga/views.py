199643427c51983d97f96284c313a65aa1f214ee
Fian-Alfian <vianalfian82@gmail.com>
2025-10-06 22:18:34 +0700

    Revert "Frist Commit"
    This reverts commit 6f01a398d377ac75195507b56375f72adc497dd8.

--- data_kelurahan/warga/views.py
+++
@@ -1,9 +0,0 @@
-from django.shortcuts import render
-from django.views.generic import ListView, DetailView
-from .models import Warga
-# Create your views here.
-class WargaListView(ListView):
-    model = Warga
-
-class WargaDetailView(DetailView):
-    model = Warga
