077e5f7ebd02531608c37089afcff3a73c6bc2c3
Fian-Alfian <vianalfian82@gmail.com>
2025-10-07 08:06:39 +0700

    secound commit-Queryset Lanjutan

--- data_kelurahan/warga/views.py
+++ data_kelurahan/warga/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.views.generic import ListView, DetailView
-from .models import Warga
+from .models import Warga, Pengaduan
 
 # Create your views here.
 class WargaListView(ListView):
@@ -8,3 +8,8 @@ class WargaListView(ListView):
 
 class WargaDetailView(DetailView):
     model = Warga
+
+class PengaduanListView(ListView):
+    model = Pengaduan
+    template_name = 'warga/pengaduan_list.html'
+    context_object_name = 'pengaduan_list'
