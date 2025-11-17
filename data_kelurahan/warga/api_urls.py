4fba4d52e3d97b0d6eb14f124e7f6acb377a5595
Fian-Alfian <vianalfian82@gmail.com>
2025-11-17 21:35:04 +0700

    Commit P-7

---
+++ data_kelurahan/warga/api_urls.py
@@ -0,0 +1,22 @@
+# from django.urls import path
+# from .views import WargaListAPIView,WargaDetailAPIView, PengaduanListAPIView, PengaduanDetailAPIView
+
+# urlpatterns = [
+#     path('warga/', WargaListAPIView.as_view(), name='api-warga-list'),
+#     path('warga/<int:pk>/', WargaDetailAPIView.as_view(), name='api-warga-detail'),
+#     path('pengaduan/', PengaduanListAPIView.as_view(), name='api-pengaduan-list'),
+#     path('pengaduan/<int:pk>/', PengaduanDetailAPIView.as_view(), name='api-pengaduan-detail'),
+# ]
+
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from .views import WargaViewSet, PengaduanViewSet
+
+# Buat sebuah router dan daftarkan ViewSet kita
+router = DefaultRouter()
+router.register(r'warga', WargaViewSet, basename='warga')
+router.register(r'pengaduan', PengaduanViewSet, basename='pengaduan')
+# URL API sekarang ditentukan secara otomatis oleh router.
+urlpatterns = [
+    path('', include(router.urls)),
+]
--- data_kelurahan/warga/views.py
+++ data_kelurahan/warga/views.py
@@ -3,6 +3,9 @@ from django.urls import reverse_lazy
 from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
 from .models import Warga, Pengaduan
 from .forms import WargaForm, PengaduanForm
+# from rest_framework.generics import ListAPIView, RetrieveAPIView
+from .serializers import WargaSerializer, PengaduanSerializer
+from rest_framework import viewsets
 
 # Create your views here.
 class WargaListView(ListView):
@@ -31,7 +34,7 @@ class PengaduanCreateView(CreateView):
 class WargaUpdateView(UpdateView):
     model = Warga
     form_class = WargaForm
-    template_name = 'warga/warga_form.html' # Kita pakai template yang sama
+    template_name = 'warga/warga_forms.html' # Kita pakai template yang sama
     success_url = reverse_lazy('warga-list')
 
 class WargaDeleteView(DeleteView):
@@ -49,3 +52,32 @@ class PengaduanDeleteView(DeleteView):
     model = Pengaduan
     template_name = 'warga/pengaduan_confirm_delete.html'
     success_url = reverse_lazy('pengaduan-list')
+
+# class WargaListAPIView(ListAPIView):
+#     queryset = Warga.objects.all()
+#     serializer_class = WargaSerializer
+
+# class WargaDetailAPIView(RetrieveAPIView):
+#     queryset = Warga.objects.all()
+#     serializer_class = WargaSerializer
+
+# class PengaduanListAPIView(ListAPIView):
+#     queryset = Warga.objects.all()
+#     serializer_class = PengaduanSerializer
+
+# class PengaduanDetailAPIView(RetrieveAPIView):
+#     queryset = Warga.objects.all()
+#     serializer_class = PengaduanSerializer
+
+class WargaViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint that allows users to be viewed or edited.
+    """
+    queryset = Warga.objects.all().order_by('-tanggal_registrasi')
+    serializer_class = WargaSerializer
+class PengaduanViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint that allows pengaduan to be viewed or edited.
+    """
+    queryset = Pengaduan.objects.all().order_by('-tanggal_lapor')
+    serializer_class = PengaduanSerializer
